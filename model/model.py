4310ac74b61269efe2865a39f9c21274d7bdf885
Changmin Choi 최창민 <cmchoi9901@gmail.com>
2019-10-16 15:34:40 +0900

    update: remove speech dependency

--- model/model.py
+++ model/model.py
@@ -7,7 +7,6 @@ class Net(nn.Module):
     def __init__(self, hp):
         super(type(self), self).__init__()
         self.hp = hp
-        self.window = torch.hann_window(window_length=hp.audio.win_length)
         raise NotImplementedError
     
     def forward(self, x):
