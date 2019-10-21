97e2efe4ad64887a5802b0480da23c4d33b8e705
Changmin Choi <cmchoi9901@gmail.com>
2019-10-21 17:24:40 +0900

    fix: fix ambiguous function name in writer.py

--- utils/writer.py
+++ utils/writer.py
@@ -8,8 +8,8 @@ class Writer(SummaryWriter):
         super(type(self), self).__init__(logdir)
         self.hp = hp
 
-    def log_training(self, train_loss, step):
+    def training_logging(self, train_loss, step):
         self.add_scalar('train_loss', train_loss, step)
 
-    def log_validation(self, test_loss, step):
+    def validation_logging(self, test_loss, step):
         self.add_scalar('test_loss', test_loss, step)
