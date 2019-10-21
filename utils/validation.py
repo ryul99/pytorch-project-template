5c61d9e3e8c0c83447f94c1102dd883babaa15eb
Changmin Choi <cmchoi9901@gmail.com>
2019-10-21 17:23:33 +0900

    fix: remove speech dependency from logger and validation

--- utils/validation.py
+++ utils/validation.py
@@ -9,25 +9,15 @@ from scipy.interpolate import interp1d
 
 def validate(model, testloader, writer, step):
     model.eval()
-
+    total_test_loss = 0
     with torch.no_grad():
-        gt = list()
-        scores = list()
-        for target, spec1, spec2 in tqdm.tqdm(testloader):
-            target = target.cuda(); spec1 = spec1.cuda(); spec2 = spec2.cuda()
-            output1 = model.inference(spec1)
-            output2 = model.inference(spec2)
-            similarity = F.cosine_similarity(output1, output2)
-            gt.append(target)
-            scores.append(similarity)
+        for model_input, target in tqdm.tqdm(test_loader):
+            target = target.cuda(); model_input = model_input.cuda()
+            output = model.inference(model_input)
+            total_test_loss += model.get_loss(output, target)
 
-        gt = torch.cat(gt, dim=0).tolist()
-        scores = torch.cat(scores, dim=0).tolist()
+        total_test_loss /= (len(test_loader.dataset) / hp.test.batch_size)
 
-        fpr, tpr, thresholds = roc_curve(gt, scores, pos_label=1)
-        eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
-        thr = interp1d(fpr, thresholds)(eer)
-
-        writer.log_validation(fpr, tpr, thresholds, eer, thr, step)
+        writer.validation_logging(total_test_loss, step)
 
     model.train()
--- utils/writer.py
+++ utils/writer.py
@@ -11,12 +11,5 @@ class Writer(SummaryWriter):
     def log_training(self, train_loss, step):
         self.add_scalar('train_loss', train_loss, step)
 
-    def log_validation(self, fpr, tpr, thresholds, eer, thr, step):
-        far = fpr
-        frr = 1.0 - tpr
-        self.add_scalar('eer', eer, step)
-        self.add_scalar('threshold', thr, step)
-        self.add_image('FAR-FRR',
-                       plt.plot_far_frr_to_numpy(thresholds, far, frr), step)
-        self.add_image('ROC',
-                       plt.plot_roc_to_numpy(far, tpr), step)
+    def log_validation(self, test_loss, step):
+        self.add_scalar('test_loss', test_loss, step)
