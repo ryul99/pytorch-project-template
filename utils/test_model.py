334cd37c84ca743d492280d06020c6dab327601c
Changmin Choi <cmchoi9901@gmail.com>
2020-05-29 19:53:22 +0900

    make test_model.py fit with refactored code

--- utils/test_model.py
+++ utils/test_model.py
@@ -3,13 +3,13 @@ import torch
 
 
 def test_model(hp, model, test_loader, writer):
-    model.eval()
+    model.net.eval()
     total_test_loss = 0
     with torch.no_grad():
         for model_input, target in tqdm.tqdm(test_loader):
             target = target.to(hp.model.device)
             model_input = model_input.to(hp.model.device)
-            output = model.inference(model_input)
+            output = model.net(model_input)
             total_test_loss += model.get_loss(output, target)
 
         total_test_loss /= len(test_loader.dataset) / hp.test.batch_size
