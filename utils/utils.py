933d037d812b7273c9cd54cf0be68eedc28425e5
Changmin Choi <cmchoi9901@gmail.com>
2020-05-29 12:15:10 +0900

    integrate utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,43 @@
 import subprocess
+import yaml
 
 
 def get_commit_hash():
     message = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
     return message.strip().decode('utf-8')
+
+
+def load_hparam(filename):
+    stream = open(filename, 'r')
+    docs = yaml.load_all(stream, Loader=yaml.Loader)
+    hparam_dict = DotDict()
+    for doc in docs:
+        for k, v in doc.items():
+            hparam_dict[k] = DotDict(v)
+    return hparam_dict
+
+
+class DotDict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, dict_=None):
+        super().__init__()
+        if dict_ is not None:
+            if not isinstance(dict_, dict):
+                raise ValueError
+            for k, v in dict_.items():
+                if isinstance(v, dict):
+                    self[k] = DotDict(v)
+                else:
+                    self[k] = v
+
+    def to_dict(self):
+        output_dict = dict()
+        for k, v in self.items():
+            if isinstance(v, DotDict):
+                output_dict[k] = v.to_dict()
+            else:
+                output_dict[k] = v
+        return output_dict
