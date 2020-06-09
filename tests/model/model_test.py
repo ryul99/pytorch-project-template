52476485be94ee3b91ab0f4cddddb12deb01bfb2
Changmin Choi <cmchoi9901@gmail.com>
2020-06-09 17:07:15 +0900

    fix setup for class and method properly

--- tests/model/model_test.py
+++ tests/model/model_test.py
@@ -9,11 +9,15 @@ from utils.utils import load_hparam
 class TestModel:
     @classmethod
     def setup_class(cls):
-        cls.hp = load_hparam("config/default.yaml")
-        cls.hp.model.device = "cpu"
-        cls.net = Net_arch(cls.hp)
-        cls.loss_f = nn.MSELoss()
-        cls.model = Model(cls.hp, cls.net, cls.loss_f)
+        cls.input_ = torch.rand(64, 10)
+        cls.gt = torch.rand(64, 1)
+
+    def setup_method(self, method):
+        self.hp = load_hparam("config/default.yaml")
+        self.hp.model.device = "cpu"
+        self.net = Net_arch(self.hp)
+        self.loss_f = nn.MSELoss()
+        self.model = Model(self.hp, self.net, self.loss_f)
 
     def test_model(self):
         assert self.model.hp == self.hp
@@ -21,17 +25,15 @@ class TestModel:
         assert self.model.loss_f == self.loss_f
 
     def test_feed_data(self):
-        input_ = torch.rand(64, 10)
-        gt = torch.rand(64, 1)
-        device_input_ = input_.to(self.hp.model.device)
-        device_gt = gt.to(self.hp.model.device)
-        self.model.feed_data(input=input_)
+        device_input_ = self.input_.to(self.hp.model.device)
+        device_gt = self.gt.to(self.hp.model.device)
+        self.model.feed_data(input=self.input_)
         assert (self.model.input == device_input_).all()
-        self.model.feed_data(GT=gt)
+        self.model.feed_data(GT=self.gt)
         assert (self.model.GT == device_gt).all()
         self.model.feed_data()
         assert self.model.input is None
         assert self.model.GT is None
-        self.model.feed_data(input=input_, GT=gt)
+        self.model.feed_data(input=self.input_, GT=self.gt)
         assert (self.model.input == device_input_).all()
         assert (self.model.GT == device_gt).all()
