238b5fae922479fbc3a3b4ee114c6dd6003e73b0
Changmin Choi <cmchoi9901@gmail.com>
2020-06-09 14:30:24 +0900

    add example codes

--- model/model_arch.py
+++ model/model_arch.py
@@ -8,7 +8,13 @@ class Net_arch(nn.Module):
     def __init__(self, hp):
         super(Net_arch, self).__init__()
         self.hp = hp
-        raise NotImplementedError
+
+        # TODO: This is example code. You should change this part as you need
+        self.fc1 = nn.Linear(10, 10)
+        self.fc2 = nn.Linear(10, 1)
 
     def forward(self, x):
-        raise NotImplementedError
+        # TODO: This is example code. You should change this part as you need
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
