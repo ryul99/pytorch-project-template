a0600f118788c8da3638bbd6afad4078774bad84
Changmin Choi <cmchoi9901@gmail.com>
2020-11-06 01:25:58 +0900

    use hydra logging (DDP is not supported yet)

--- tests/test_case.py
+++ tests/test_case.py
@@ -4,7 +4,7 @@ import os
 import pathlib
 import shutil
 import tempfile
-from utils.logger import make_logger
+import logging
 from hydra.experimental import initialize, compose
 
 TEST_DIR = tempfile.mkdtemp(prefix="project_tests")
@@ -14,23 +14,23 @@ class ProjectTestCase:
     def setup_method(self):
         # set log/checkpoint dir
         self.TEST_DIR = pathlib.Path(TEST_DIR)
-        self.log_dir = (self.TEST_DIR / "logs").resolve()
-        self.chkpt_dir = (self.TEST_DIR / "chkpt").resolve()
+        self.working_dir = self.TEST_DIR
+        chkpt_dir = (self.TEST_DIR / "chkpt").resolve()
         os.makedirs(self.TEST_DIR, exist_ok=True)
-        os.makedirs(self.log_dir, exist_ok=True)
-        os.makedirs(self.chkpt_dir, exist_ok=True)
+        os.makedirs(chkpt_dir, exist_ok=True)
 
         # set cfg
         with initialize(config_path="../config"):
-            self.cfg = compose(config_name="default")
-        self.cfg.model.device = "cpu"
-        self.cfg.log.log_dir = str(self.log_dir)
-        self.cfg.log.chkpt_dir = str(self.chkpt_dir)
-        self.cfg.log.use_wandb = False
-        self.cfg.log.use_tensorboard = False
+            self.cfg = compose(
+                config_name="train", overrides=[f"train.working_dir={self.working_dir}"]
+            )
+        self.cfg.train.model.device = "cpu"
+        self.cfg.train.log.chkpt_dir = str(chkpt_dir)
+        self.cfg.train.log.use_wandb = False
+        self.cfg.train.log.use_tensorboard = False
 
         # set logger
-        self.logger = make_logger(self.cfg)
+        self.logger = logging.getLogger(os.path.basename(__file__))
 
     def teardown_method(self):
         shutil.rmtree(self.TEST_DIR)
